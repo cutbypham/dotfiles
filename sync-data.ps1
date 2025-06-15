0a71d728f00ac952314c7546efa0acb363958a03
cutbypham <cutbypham@gmail.com>
2025-06-15 17:37:27 +0700

    Auto commit

--- sync-data.ps1
+++ sync-data.ps1
@@ -20,3 +20,14 @@ auto-git-commit
 
 cd C:\Users\master\repos\davinci-resolve\
 auto-git-commit
+
+$LogFile = "C:\Users\master\Documents\sync-data-log.txt"
+
+function Write-Log {
+    param([string]$Message)
+    $timestamp = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+    $entry = "$timestamp $Message"
+    Add-Content -Path $LogFile -Value $entry -Encoding UTF8
+}
+
+Write-Log "Script finished"
